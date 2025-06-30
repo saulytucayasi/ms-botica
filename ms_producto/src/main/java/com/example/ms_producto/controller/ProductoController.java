2e32810f10290bf7ed9e225d1d9b4c2d8b15a518
Saul Ytucayasi <saul.ytucayasi@gmail.com>
2025-06-29 22:27:14 -0500

    dist

--- ms_producto/src/main/java/com/example/ms_producto/controller/ProductoController.java
+++ ms_producto/src/main/java/com/example/ms_producto/controller/ProductoController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/producto")
+@RequestMapping("/productos")
 public class ProductoController {
     @Autowired
     private ProductoService productoService;
